795a15f26ca7f3686b1caedc362e5181ab34f41b
Alden Walker <awalker@caltech.edu>
2013-10-27 13:16:55 -0500

    redoing with not simplex types

--- branched.cc
+++ branched.cc
@@ -6,62 +6,58 @@
 
 #include "branched.h"
 
-
-/****************************************************************************
- * Simplex                                                                  
- ****************************************************************************/
-Simplex::Simplex() {
-  dim = 0;
-  bd.resize(0);
+Vertex::Vertex() {
   in_bd_of.resize(0);
 }
 
-Simplex::Simplex(int n) {
-  dim = n;
-  bd = std::vector<SignedInd>(n, 0);
-  in_bd_od.resize(0);
-}
-
-Simplex::Simplex(std::vector<SignedInd>& bd) {
-  dim = bd.size();
-  this->bd = bd;
-  in_bd_of.resize(0);
-}
-
-std::string Simplex::ihat_string(int i) {
-  std::ostringstream ans;
-  for (int j=0; j<dim; ++j) {
-    if (j==i) continue;
-    ans << j;
-  }
-  return ans.string();
-}
-
-int Simplex::dimension() {
-  return dim;
-}
-
-SignedInd& Simplex::boundary(int i) {
-  return bd[i];
-}
-
-SignedInd& Simplex::operator[](int i) {
-  return bd[i];
-}
-
-SignedInd& Simplex::in_boundary_of(int i) {
-  return in_bd_of[i];
-}
-
-std::ostream& operator<<(std::ostream& os, Simplex& S) {
-  os << S.dim << "s{";
-  for (int i=0; i<dim; ++i) {
-    os << ihat_string(i) << ": " << bd[i];
-    if (i<dim) os << ", ";
+std::ostream& operator<<(std::ostream& os, Vertex& v) {
+  os << "V(); {";
+  for (int i=0; i<(int)v.in_bd_of.size(); ++i) {
+    os << v.in_bd_of[i];
+    if (i<v.in_bd_of.size()-1) os << ", ";
   }
   os << "}";
   return os;
 }
+
+Edge::Edge() {
+  bd.resize(2,0);
+  in_bd_of.resize(0);
+}
+
+SignedInd& Edge::operator[](int i) {
+  return bd[i];
+}
+
+std::ostream& operator<<(std::ostream& os, Edge& e) {
+  os << "E(" << e.bd[0] << "," << e.bd[1] << "); {"
+  for (int i=0; i<(int)e.in_bd_of.size(); ++i) {
+    os << e.in_bd_of[i];
+    if (i<e.in_bd_of.size()-1) os << ", ";
+  }
+  os << "}";
+  return os;
+}
+
+Triangle::Triangle() {
+  bd.resize(3,0);
+  in_bd_of.resize(0);
+}
+
+SignedInd& Triangle::operator[](int i) {
+  return bd[i];
+}
+
+std::ostream& operator<<(std::ostream& os, Triangle& t) {
+  os << "T(" << t.bd[0] << "," << t.bd[1] << "," << t.bd[2] << "); {"
+  for (int i=0; i<(int)t.in_bd_of.size(); ++i) {
+    os << t.in_bd_of[i];
+    if (i<t.in_bd_of.size()-1) os << ", ";
+  }
+  os << "}";
+  return os;
+}
+
 
 /*****************************************************************************
  * Triangulation                                                             
@@ -91,26 +87,86 @@ void Triangulation::print(ostream& os) {
   }
 }
 
+
+// Given a integral weight vector on the triangles, produce a 
+// branched surface triangulation containing appropriate duplicates 
+// of the triangles in the weights.  Note any unused simplices are 
+// removed, so the indices of everything can be different
 Triangulation Triangulation::branched_surface_from_vector(std::vector<int>& weights) {
   Triangulation new_T;
-  //use all the vertices and edges which are contained in the weighted triangles
-  //we can just record which edges are used, and then only use those
-  //we need to redo indices and stuff on the other simplices though
-  new_T.vertices.resize(0);
-  new_T.edges.resize(0);
+
+  //Determine which edges are used, and figure out what the new 
+  //indices of the used edges will be
+  int num_edges_used = 0;
+  int num_vertices_used = 0;
   std::vector<bool> is_edge_used(edges.size(), false);
   for (int i=0; i<(int)weights.size(); ++i) {
-    for (int j=0; j<edges[i].dim(); ++j) {
-      is_edge_used[ abs(edges[i].bd[j]) ] = true;
+    if (weights[i] == 0) continue;
+    for (int j=0; j<triangles[i].dim; ++j) {
+      is_edge_used[ abs(triangles[i][j]) ] = true;
     }
   }
   std::vector<int> edge_index_translation_table(edges.size(), 0);
   int current_new_index = 1;
-  for (int i=0; 
+  for (int i=1; i<(int)edges.size(); ++i) {
+    if (is_edge_used[i]) {
+      edge_index_translation_table[i] = current_new_index;
+      ++current_new_index;
+    }
+  }
+  num_edges_used = current_new_index-2;
   
+  //Determine which vertices are used, and figure out what the new 
+  //indices will be
   std::vector<bool> is_vertex_used(vertices.size(), false);
-  for (int i=0; i<(int)edges.size(); ++i) {
-    if (is_edge_used[i] && 
+  for (int i=1; i<(int)edges.size(); ++i) {
+    if (is_edge_used[i]) {
+      is_vertex_used[ abs(edges[i][0]) ] = true;
+      is_vertex_used[ abs(edges[i][1]) ] = true;
+    }
+  }
+  std::vector<int> vertex_index_translation_table(vertices.size(), 0);
+  current_new_index = 1;
+  for (int i=1; i<(int)vertices.size(); ++i) {
+    if (is_vertex_used[i]) {
+      vertex_index_translation_table[i] = current_new_index;
+      ++current_new_index;
+    }
+  }
+  num_vertices_used = current_new_index-2;
+  
+  //Create the new lists of vertices and edges
+  //record which edges are incident to which vertices
+  new_T.vertices.resize(num_vertices_used+1, Simplex(0));
+  new_T.edges.resize(num_edges_used+1, Simplex(1));
+  for (int i=1; i<(int)edges.size(); ++i) {
+    int ei = edge_index_translation_table[i];
+    if (ei == 0) continue;
+    int v0i = vertex_index_translation_table[ edges[i][0] ];
+    new_T.edges[i][0] = v0i;
+    new_T.vertices[v0i].in_bd_of.push_back(ei);
+    int v1i = vertex_index_translation_table[ edges[i][1] ];
+    new_T.edges[i][1] = v1i;
+    new_T.vertices[v1i].in_bd_of.push_back(-ei);
+  }
+  
+  //Create the new list of triangles
+  new_T.triangles.resize(1);
+  for (int i=0; i<(int)weights.size(); ++i) {
+    Simplex temp_simp(2);
+    int ei[3];
+    for (int j=0; j<3; ++j) {
+      ei[j] = sgn(triangles[i][j])*edge_index_translation_table[ abs(triangles[i][j]) ];
+      temp_simp[j] = ei[j];
+    }
+    for (int j=0; j<weights[i]; ++j) {
+      int this_t_ind = new_T.triangles.size();
+      new_T.edges[ 
+      
+  
+  
+  
+  
   
   
 }
--- branched.h
+++ branched.h
@@ -8,50 +8,48 @@
 
 typedef SignedInd int;
 
-/****************************************************************************
- * Simplex                                                                  
- ****************************************************************************/
+int sgn(int x) { 
+  return ((x > 0) - (x < 0));
+}
 
-//the boundary is listed in standard form b[i] = \hat{i}, 
-//i.e. the boundary simplex on which i is omitted
-class Simplex {
-  private:
-    int dim;
-    std::vector<SignedInd> bd;
-    std::vector<SignedInd> in_bd_of;
-  public:
-    Simplex();
-    Simplex(int n);
-    Simplex(std::vector<SignedInd>& bd);
-    
-    int dim();
-    SignedInd& boundary(int i);
-    SignedInd& operator[](int i);
-    SignedInd& in_boundary_of(int i);
-    std::string ihat_string(int i); //returns the appropriate string
-    
-    friend std::ostream& operator<<(std::ostream& os, Simplex& S);
+// we could use a standard simplex type, but the boundaries 
+// are confusing, so we'll use different types
+struct Vertex {
+  Vertex();
+  std::vector<SignedInd> in_bd_of;
 };
-    
+std::ostream& operator<<(std::ostream& os, Vertex& v);
+
+struct Edge {
+  Edge();
+  std::vector<SignedInd> bd;
+  std::vector<SignedInd> in_bd_of;
+  SignedInd& operator[](int i);
+};
+std::ostream& operator<<(std::ostream& os, Edge& e);
+
+struct Triangle {
+  Triangle();
+  std::vector<SignedInd> bd;
+  std::vector<SignedInd> in_bd_of;
+  SignedInd& operator[](int i);
+};
+std::ostream& operator<<(std::ostream& os, Triangle& t);
+
 
 /*****************************************************************************
  * Triangulation                                                             
  *****************************************************************************/
 //all lists in here are ONE-BASED, so that SignedInds are easier
-class Triangulation {
-  private:
+struct Triangulation {
     std::vector<Simplex> triangles;
     std::vector<Simplex> edges;
     std::vector<Simplex> vertices;
 
-  public:
     Triangulation();
     void read_file(std::string filename);
     void write_file(std::string filename);
     void print(ostream& os);
-    Simplex& triangle(int i);
-    Simplex& edge(int i);
-    Simplex& vertex(int i);
   
     Triangulation branched_surface_from_vector(std::vector<int>& weights);
     Triangulation resolve_branched_surface();
