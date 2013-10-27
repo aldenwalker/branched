f8728d216aa4b5cd4be3a8f21f4797a5a8e2afb1
Alden Walker <awalker@caltech.edu>
2013-10-26 19:17:35 -0500

    working on weight vector

--- branched.cc
+++ branched.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <stringstream>
+#include <cstdlib>
 
 #include "branched.h"
 
@@ -77,20 +78,40 @@ void Triangulation::write_file(std::string filename) {}
 
 void Triangulation::print(ostream& os) {
   os << "Vertices (" << vertices.size()-1 << "):\n";
-  for (int i=1; i<=vertices.size(); ++i) {
+  for (int i=1; i<(int)vertices.size(); ++i) {
     os << i << ": " << vertices[i] << "\n";
   }
-  os << "Edges (" <<edges.size()-1 << ")\n";
-  for (int i=1; i<=edges.size(); ++i) {
+  os << "Edges (" << edges.size()-1 << ")\n";
+  for (int i=1; i<(int)edges.size(); ++i) {
     os << i << ": " << edges[i] << "\n";
   }
-  os << "Triangles (" <<triangles.size()-1 << ")\n";
-  for (int i=1; i<=triangles.size(); ++i) {
+  os << "Triangles (" << triangles.size()-1 << ")\n";
+  for (int i=1; i<(int)triangles.size(); ++i) {
     os << i << ": " << triangles[i] << "\n";
   }
 }
 
 Triangulation Triangulation::branched_surface_from_vector(std::vector<int>& weights) {
+  Triangulation new_T;
+  //use all the vertices and edges which are contained in the weighted triangles
+  //we can just record which edges are used, and then only use those
+  //we need to redo indices and stuff on the other simplices though
+  new_T.vertices.resize(0);
+  new_T.edges.resize(0);
+  std::vector<bool> is_edge_used(edges.size(), false);
+  for (int i=0; i<(int)weights.size(); ++i) {
+    for (int j=0; j<edges[i].dim(); ++j) {
+      is_edge_used[ abs(edges[i].bd[j]) ] = true;
+    }
+  }
+  std::vector<int> edge_index_translation_table(edges.size(), 0);
+  int current_new_index = 1;
+  for (int i=0; 
+  
+  std::vector<bool> is_vertex_used(vertices.size(), false);
+  for (int i=0; i<(int)edges.size(); ++i) {
+    if (is_edge_used[i] && 
+  
   
 }
 
